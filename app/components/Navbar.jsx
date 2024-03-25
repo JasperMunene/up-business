8db46ef1e43022c3ebd681abece8da61cba140f2
JasperMunene <jaspermunene2022@gmail.com>
2024-03-26 02:23:58 +0300

    completed navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { FaArrowUp } from 'react-icons/fa';
+import Link from 'next/link';
+import { Button } from "@/components/ui/button"
+
+
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+   <nav className='w-full pl-8 pr-10 pt-3 flex justify-between items-center'>
+   <Link href="/" className='inline-block'>
+        <h2 className='flex gap-1 text-lg py-2 m-4 align-middle'>
+            <FaArrowUp className='text-[#FF87B0]' />
+            <span className='text-[#7C76BB] px-1 font-bold'>UP BUSINESS</span>
+        </h2>
+    </Link>
+   <div className='flex gap-7  items-center'>
+    <ul className="flex list-none gap-5 text-lg">
+        <li>
+            <a className='inline-block px-3 py-4 no-underline outline-none hover:no-underline text-[#7C76BB] hover:text-[#FF87B0]' href='#'>Home</a>
+        </li>
+        <li>
+            <a className='inline-block px-3 py-4 no-underline outline-none hover:no-underline text-[#7c76bb] hover:text-[#FF87B0]' href='#'>How we work</a>
+        </li>
+        <li>
+            <a className='inline-block px-3 py-4 no-underline outline-none hover:no-underline text-[#7C76BB] hover:text-[#FF87B0]' href='#'>Our Team</a>
+        </li>
+        <li>
+            <a className='inline-block px-3 py-4 no-underline outline-none hover:no-underline text-[#7C76BB] hover:text-[#FF87B0]' href='#'>Pricing</a>
+        </li>
+       
+       </ul>
+    <Button>Book a call</Button>
+    </div>
+   
+   </nav>
   )
 }
 
--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
       <ToastContainer />
-      <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+      <main>
         <Navbar />
         {children}
       </main>
